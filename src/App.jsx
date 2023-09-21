b1f13430981a1d202ee15a95ef3f2d9ddc5b7c40
asif-hosain <asifhdoha@gmail.com>
2023-09-21 22:23:34 +0600

    RefForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import RefForm from './components/RefForm/RefForm'
 import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <>
    
       <h1>Vite + React</h1>
-      <SimpleForm></SimpleForm>
+      {/* <SimpleForm></SimpleForm> */}
+      <RefForm></RefForm>
 
     </>
   )
